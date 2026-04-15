7afb45695cf34650d7baa264d44c810720747511
vishanthkannan <vishanthkannan777@gmail.com>
2026-04-16 00:56:09 +0530

    final fix using tmp

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,19 @@
 
 echo "Deploying to Kubernetes..."
 
+# Copy files to temporary folder
+cp deployment.yaml /tmp/deployment.yaml
+cp service.yaml /tmp/service.yaml
+
+# Run kubectl container with copied files
 docker run --rm \
-  -v /var/jenkins_home/workspace/devops-final:/app \
+  -v /tmp:/app \
   -v /root/.kube:/root/.kube \
-  --network host \
   bitnami/kubectl:latest \
   apply -f /app/deployment.yaml
 
 docker run --rm \
-  -v /var/jenkins_home/workspace/devops-final:/app \
+  -v /tmp:/app \
   -v /root/.kube:/root/.kube \
-  --network host \
   bitnami/kubectl:latest \
   apply -f /app/service.yaml
