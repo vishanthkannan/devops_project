8ea1dd249fbd2bb2b71cfed2c0b3352b03c34bc3
vishanthkannan <vishanthkannan777@gmail.com>
2026-04-16 00:49:34 +0530

    fix path issue

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 echo "Deploying to Kubernetes..."
 
 docker run --rm \
-  -v ~/.kube:/root/.kube \
-  -v $(pwd):/app \
+  -v /var/jenkins_home/workspace/devops-final:/app \
+  -v /root/.kube:/root/.kube \
   bitnami/kubectl:latest \
   apply -f /app/deployment.yaml
 
 docker run --rm \
-  -v ~/.kube:/root/.kube \
-  -v $(pwd):/app \
+  -v /var/jenkins_home/workspace/devops-final:/app \
+  -v /root/.kube:/root/.kube \
   bitnami/kubectl:latest \
   apply -f /app/service.yaml
