3561ae7bbfb227ad1365f496d8b0d94d0da43773
vishanthkannan <vishanthkannan777@gmail.com>
2026-04-16 00:51:47 +0530

    final workspace fix

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,17 @@
 
 echo "Deploying to Kubernetes..."
 
+echo "Workspace path is: $WORKSPACE"
+ls -l $WORKSPACE   # debug (very important)
+
 docker run --rm \
-  -v /var/jenkins_home/workspace/devops-final:/app \
+  -v $WORKSPACE:/app \
   -v /root/.kube:/root/.kube \
   bitnami/kubectl:latest \
   apply -f /app/deployment.yaml
 
 docker run --rm \
-  -v /var/jenkins_home/workspace/devops-final:/app \
+  -v $WORKSPACE:/app \
   -v /root/.kube:/root/.kube \
   bitnami/kubectl:latest \
   apply -f /app/service.yaml
