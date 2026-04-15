5eac1549712314c88b308a47f4182133117f54f3
vishanthkannan <vishanthkannan777@gmail.com>
2026-04-16 01:07:17 +0530

    fix-4

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,5 @@
 
 echo "Deploying to Kubernetes..."
 
-# Copy files to temporary folder
-cp deployment.yaml /tmp/deployment.yaml
-cp service.yaml /tmp/service.yaml
-
-# Run kubectl container with copied files
-docker run --rm \
-  -v /tmp:/app \
-  -v /root/.kube:/root/.kube \
-  bitnami/kubectl:latest \
-  apply -f /app/deployment.yaml
-
-docker run --rm \
-  -v /tmp:/app \
-  -v /root/.kube:/root/.kube \
-  bitnami/kubectl:latest \
-  apply -f /app/service.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml   
